4ef7e066e6964a3f613a7f2316a385f6b6e193b1
Sachin Shelke <shelkesp1@gmail.com>
2022-11-09 13:32:01 +0530

    carousel_images+component

--- src/scenes/home/Home.jsx
+++ src/scenes/home/Home.jsx
@@ -1,7 +1,13 @@
+import { Box } from "@mui/material";
 import React from "react";
+import MainCarousel from "./MainCarousel";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Box>
+      <MainCarousel />
+    </Box>
+  );
 };
 
 export default Home;
