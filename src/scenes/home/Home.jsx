ce833b3e5ee99d870704b1b4f758dac77f47f7a2
Sachin Shelke <shelkesp1@gmail.com>
2022-11-09 15:50:16 +0530

    products_listing

--- src/scenes/home/Home.jsx
+++ src/scenes/home/Home.jsx
@@ -1,11 +1,13 @@
 import { Box } from "@mui/material";
 import React from "react";
 import MainCarousel from "./MainCarousel";
+import ShoppingList from "./ShoppingList";
 
 const Home = () => {
   return (
     <Box>
       <MainCarousel />
+      <ShoppingList />
     </Box>
   );
 };
