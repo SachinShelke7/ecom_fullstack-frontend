898accab2e49855cd6f459703f08362be0aa4210
Sachin Shelke <shelkesp1@gmail.com>
2022-11-21 10:23:45 +0530

    checkout_validation_ui_submit

--- src/scenes/checkout/Checkout.jsx
+++ src/scenes/checkout/Checkout.jsx
@@ -1,7 +1,202 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { Box, Button, Steps, Step, Stepper, StepLabel } from "@mui/material";
+import { Formik } from "formik";
+import * as yup from "yup";
+import { shades } from "../../theme";
+import Shipping from "./Shipping";
+import Payment from "./Payment";
+
+const initialValues = {
+  billingAddress: {
+    firstName: "",
+    lastName: "",
+    country: "",
+    street1: "",
+    street2: "",
+    city: "",
+    state: "",
+    zipCode: "",
+  },
+  shippingAddress: {
+    isSameAddress: true,
+    firstName: "",
+    lastName: "",
+    country: "",
+    street1: "",
+    street2: "",
+    city: "",
+    state: "",
+    zipCode: "",
+  },
+  email: "",
+  phoneNumber: "",
+};
+
+const checkoutSchema = [
+  yup.object().shape({
+    billingAddress: yup.object().shape({
+      firstName: yup.string().required("required"),
+      lastName: yup.string().required("required"),
+      country: yup.string().required("required"),
+      street1: yup.string().required("required"),
+      street2: yup.string(),
+      city: yup.string().required("required"),
+      state: yup.string().required("required"),
+      zipCode: yup.string().required("required"),
+    }),
+    shippingAddress: yup.object().shape({
+      isSameAddress: yup.boolean(),
+      firstName: yup.string().when("isSameAddress", {
+        is: false,
+        then: yup.string().required("required"),
+      }),
+      lastName: yup.string().when("isSameAddress", {
+        is: false,
+        then: yup.string().required("required"),
+      }),
+      country: yup.string().when("isSameAddress", {
+        is: false,
+        then: yup.string().required("required"),
+      }),
+      street1: yup.string().when("isSameAddress", {
+        is: false,
+        then: yup.string().required("required"),
+      }),
+      street2: yup.string(),
+      city: yup.string().when("isSameAddress", {
+        is: false,
+        then: yup.string().required("required"),
+      }),
+      state: yup.string().when("isSameAddress", {
+        is: false,
+        then: yup.string().required("required"),
+      }),
+      zipCode: yup.string().when("isSameAddress", {
+        is: false,
+        then: yup.string().required("required"),
+      }),
+    }),
+  }),
+  yup.object().shape({
+    email: yup.string().required("required"),
+    phoneNumber: yup.string().required("required"),
+  }),
+];
 
 const Checkout = () => {
-  return <div>Checkout</div>;
+  const [activeStep, setActiveStep] = useState(0);
+  const cart = useSelector((state) => state.cart.cart);
+  const isFirst = activeStep === 0;
+  const isSecond = activeStep === 1;
+
+  const handleFormSubmit = async (values, actions) => {
+    setActiveStep(activeStep + 1);
+
+    // billing address on shipping if true
+    if (isFirst && values.shippingAddress.isSameAddress) {
+      actions.setFieldValue("shippingAddress", {
+        ...values.billingAddress,
+        isSameAddress: true,
+      });
+    }
+
+    if (isSecond) {
+      // makePayment(values);
+    }
+
+    actions.setTouched({});
+  };
+
+  return (
+    <Box width="80%" m="100px auto">
+      <Stepper activeStep={activeStep} sx={{ m: "20px 0" }}>
+        <Step>
+          <StepLabel>Billing</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Payment</StepLabel>
+        </Step>
+      </Stepper>
+      <Box>
+        <Formik
+          onSubmit={handleFormSubmit}
+          initialValues={initialValues}
+          validationSchema={checkoutSchema[activeStep]}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleBlur,
+            handleChange,
+            handleSubmit,
+            setFieldValue,
+          }) => (
+            <form onSubmit={handleSubmit}>
+              {isFirst && (
+                <Shipping
+                  values={values}
+                  errors={errors}
+                  touched={touched}
+                  handleBlur={handleBlur}
+                  handleChange={handleChange}
+                  handleSubmit={handleSubmit}
+                  setFieldValue={setFieldValue}
+                />
+              )}
+              {isSecond && (
+                <Payment
+                  values={values}
+                  errors={errors}
+                  touched={touched}
+                  handleBlur={handleBlur}
+                  handleChange={handleChange}
+                  handleSubmit={handleSubmit}
+                  setFieldValue={setFieldValue}
+                />
+              )}
+              <Box display="flex" justifyContent="space-between" gap="50px">
+                {isSecond ? (
+                  <Button
+                    fullWidth
+                    color="primary"
+                    variant="contained"
+                    sx={{
+                      backgroundColor: shades.primary[200],
+                      boxShadow: "none",
+                      color: "white",
+                      borderRadius: 0,
+                      padding: "15px 40px",
+                    }}
+                    onClick={() => setActiveStep(activeStep - 1)}
+                  >
+                    Back
+                  </Button>
+                ) : null}
+                <Button
+                  fullWidth
+                  type="submit"
+                  color="primary"
+                  variant="contained"
+                  sx={{
+                    backgroundColor: shades.primary[400],
+                    boxShadow: "none",
+                    color: "white",
+                    borderRadius: 0,
+                    padding: "15px 40px",
+                  }}
+                  onClick={() => setActiveStep(activeStep + 1)}
+                >
+                  {isFirst ? "Next" : "Place Order"}
+                </Button>
+              </Box>
+            </form>
+          )}
+        </Formik>
+      </Box>
+    </Box>
+  );
 };
 
 export default Checkout;
--- src/scenes/itemDetails/ItemDetails.jsx
+++ src/scenes/itemDetails/ItemDetails.jsx
@@ -28,8 +28,18 @@ const ItemDetails = () => {
     );
     const itemJson = await item.json();
     setItem(itemJson.data);
-    if (itemJson.error.status === 404) {
-      setItem("return");
+    if (itemJson?.error?.status === 404) {
+      toast.error("Oops Something went wrong!!", {
+        position: "bottom-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+      navigate("/");
     }
   }
 
@@ -45,90 +55,81 @@ const ItemDetails = () => {
   useEffect(() => {
     getItem();
     getItems();
-    if (item === "return") {
-      toast.error("Oops Something went wrong!!", {
-        position: "bottom-right",
-        autoClose: 2000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-      });
-      navigate("/");
-    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [itemId, item]);
+  }, [itemId]);
 
   return (
     <Box width="80%" m="80px auto">
-      <Box display="flex" flexWrap="wrap" columnGap="40px">
-        {/* image */}
-        <Box flex="1 1 40%" mb="40px">
-          <img
-            alt={item?.name}
-            width="100%"
-            height="100%"
-            src={`http://localhost:1337${item?.attributes?.image?.data?.attributes?.formats?.medium?.url}`}
-            style={{ objectFit: "contain" }}
-          />
-        </Box>
-        {/* actions */}
-        <Box flex="1 1 50%" mb="40px">
-          <Box display="flex" justifyContent="space-between">
-            <Box>Home/Item</Box>
-            <Box>Prev Next</Box>
+      {item ? (
+        <Box display="flex" flexWrap="wrap" columnGap="40px">
+          {/* image */}
+          <Box flex="1 1 40%" mb="40px">
+            <img
+              alt={item?.name}
+              width="100%"
+              height="100%"
+              src={`http://localhost:1337${item?.attributes?.image?.data?.attributes?.formats?.medium?.url}`}
+              style={{ objectFit: "contain" }}
+            />
           </Box>
-          <Box m="65px 0 25px 0">
-            <Typography variant="h3">{item?.attributes?.name}</Typography>
-            <Typography>{item?.attributes?.price}</Typography>
-            <Typography sx={{ mt: "20px" }}>
-              {item?.attributes?.longDescription}
-            </Typography>
-          </Box>
-
-          {/* count and button */}
-          <Box display="flex" alignItems="center" minHeight="50px">
-            <Box
-              display="flex"
-              alignItems="center"
-              border={`1.5px solid ${shades.neutral[300]}`}
-              mr="20px"
-              o="2px 5px"
-            >
-              <IconButton onClick={() => setCount(Math.max(count - 1, 1))}>
-                <Remove />
-              </IconButton>
-              <Typography sx={{ p: "0 5px" }}>{count}</Typography>
-              <IconButton onClick={() => setCount(count + 1)}>
-                <Add />
-              </IconButton>
+          {/* actions */}
+          <Box flex="1 1 50%" mb="40px">
+            <Box display="flex" justifyContent="space-between">
+              <Box>Home/Item</Box>
+              <Box>Prev Next</Box>
             </Box>
-            <Button
-              sx={{
-                backgroundColor: "#222222",
-                color: "white",
-                borderRadius: "0",
-                minWidth: "150px",
-                padding: "10px 40px",
-                ":hover": { backgroundColor: "#333333" },
-              }}
-              onClick={() => dispatch(addToCart({ item: { ...item, count } }))}
-            >
-              ADD TO CART
-            </Button>
-          </Box>
-
-          <Box>
-            <Box m="20px 0 5px 0" display="flex">
-              <FavoriteBorderOutlined />
-              <Typography sx={{ ml: "5px" }}>ADD TO WISHLIST</Typography>
+            <Box m="65px 0 25px 0">
+              <Typography variant="h3">{item?.attributes?.name}</Typography>
+              <Typography>{item?.attributes?.price}</Typography>
+              <Typography sx={{ mt: "20px" }}>
+                {item?.attributes?.longDescription}
+              </Typography>
             </Box>
-            <Typography>CATEGORIES: {item?.attributes?.category}</Typography>
+
+            {/* count and button */}
+            <Box display="flex" alignItems="center" minHeight="50px">
+              <Box
+                display="flex"
+                alignItems="center"
+                border={`1.5px solid ${shades.neutral[300]}`}
+                mr="20px"
+                o="2px 5px"
+              >
+                <IconButton onClick={() => setCount(Math.max(count - 1, 1))}>
+                  <Remove />
+                </IconButton>
+                <Typography sx={{ p: "0 5px" }}>{count}</Typography>
+                <IconButton onClick={() => setCount(count + 1)}>
+                  <Add />
+                </IconButton>
+              </Box>
+              <Button
+                sx={{
+                  backgroundColor: "#222222",
+                  color: "white",
+                  borderRadius: "0",
+                  minWidth: "150px",
+                  padding: "10px 40px",
+                  ":hover": { backgroundColor: "#333333" },
+                }}
+                onClick={() =>
+                  dispatch(addToCart({ item: { ...item, count } }))
+                }
+              >
+                ADD TO CART
+              </Button>
+            </Box>
+
+            <Box>
+              <Box m="20px 0 5px 0" display="flex">
+                <FavoriteBorderOutlined />
+                <Typography sx={{ ml: "5px" }}>ADD TO WISHLIST</Typography>
+              </Box>
+              <Typography>CATEGORIES: {item?.attributes?.category}</Typography>
+            </Box>
           </Box>
         </Box>
-      </Box>
+      ) : null}
 
       {/* Information */}
       <Box m="20px 0">
